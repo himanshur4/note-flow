f4a77eb60fe03ddaaa31909902f356253f6a4403
Himanshu Raj <hraj808404@gmail.com>
2025-05-29 12:20:30 +0530

    Backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from "express"
 import notesRouter from "./src/routes/notesRouter.js";
 import "dotenv/config";
-import connectDB from "./src/lib/db.js";
+import connectDB from "./src/config/db.js";
+import rateLimiter from "./src/middleware/rateLimiter.js";
 
-const app=express();
-const PORT=process.env.PORT||5001;
-
-connectDB();
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(rateLimiter);
 
-app.use("/api/notes",notesRouter);
+app.use("/api/notes", notesRouter);
 
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT:${PORT}`);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on PORT:${PORT}`);
+    });
 });
+
--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,8 +1,8 @@
 import Note from "../models/Note.js";
 
-export const getAllNotes=async(req,res)=>{
+export const getAllNotes=async(_,res)=>{
     try {
-        const notes=await Note.find();
+        const notes=await Note.find().sort({createdAt:-1});
         res.status(200).json(notes);
     } catch (error) {
         console.error("Error in getAllNotes controller ",error.message);
