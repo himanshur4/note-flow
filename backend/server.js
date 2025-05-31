d0ece7317e6fdd702a3168eeda89c91a5562538d
Himanshu Raj <hraj808404@gmail.com>
2025-05-31 07:28:26 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import "dotenv/config";
 import cors from "cors";
+import path from "path";
 
 import connectDB from "./src/config/db.js";
 import rateLimiter from "./src/middleware/rateLimiter.js";
@@ -8,16 +9,28 @@ import notesRouter from "./src/routes/notesRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors(
-    {
-        origin: "http://localhost:5173"
-    }
-));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173"
+        }
+    ));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+
+    });
+}
 
 
 connectDB().then(() => {
